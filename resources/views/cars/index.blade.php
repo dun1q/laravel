b1fb9565f62e085510ac8b570cc7a873bdeaa39a
dun1q <anufrievdanil09@yandex.ru>
2025-12-10 18:55:27 +0500

    fix

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -5,7 +5,6 @@
 @section('content')
 <div class="d-flex justify-content-between align-items-center mb-4">
     <h1 class="text-danger">Объявления</h1>
-    <!-- Кнопка "Добавить" удалена — теперь есть "Загрузить" в шапке -->
 </div>
 
 @if (session('success'))
